414874638befca48ffdf7bdfea565949d2a754a9
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-24 13:46:27 +0100

    Added main parts of program

--- arith_format.py
+++ arith_format.py
@@ -1,3 +1,12 @@
+import re
+
 user_input = str(input("Input up to 5 arithmetic problems: "))
+if len(user_input) > 65 or user_input.count(",") > 4:
+    print("Error: Too many problems.")
+elif bool(re.search('[/*&$@#|:.;!?]', user_input)):
+    print("Error: Operator must be '+' or '-'.")
+elif bool(re.search('[a-zA-Z]', user_input)):
+    print("Error: Numbers must only contain digits.")
+elif bool(re.search())
 problems = user_input.split(", ")
 print(problems)
